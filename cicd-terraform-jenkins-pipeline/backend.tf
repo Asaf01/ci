dcfea48f7a1e9931994bc38402cbb75f6d19a656
asaf <bendrorasaf@gmail.com>
2024-06-18 15:49:02 +0300

    onefolder

--- cicd-terraform-jenkins-pipeline/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "cicd-terraform-aws-jenkins-pipeline"
-    key    = "terraform.tfstate"
-    region = "eu-west-2"
-  }
-}
--- cicd-terraform-jenkins-pipeline/modules/security/output.tf
+++
@@ -1,3 +0,0 @@
-output "vpc_security_group_ids" {
-  value = aws_security_group.app_instance_sg.id
-}
